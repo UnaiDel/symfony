016c3f141a797f6a2864f6bb17ff5982f6a99b10
unailexprogram <unai@lexprogram.com>
2023-01-05 13:36:43 +0100

    first commit

---
+++ symfony-tutorial-3/src/Entity/Pedido.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PedidoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PedidoRepository::class)
+ */
+class Pedido
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $referencia;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Cliente::class, inversedBy="pedidos")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $cliente;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReferencia(): ?string
+    {
+        return $this->referencia;
+    }
+
+    public function setReferencia(string $referencia): self
+    {
+        $this->referencia = $referencia;
+
+        return $this;
+    }
+
+    public function getCliente(): ?Cliente
+    {
+        return $this->cliente;
+    }
+
+    public function setCliente(?Cliente $cliente): self
+    {
+        $this->cliente = $cliente;
+
+        return $this;
+    }
+}
---
+++ symfony-tutorial-3/src/Entity/Producto.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductoRepository::class)
+ */
+class Producto
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $referencia;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $precio;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReferencia(): ?string
+    {
+        return $this->referencia;
+    }
+
+    public function setReferencia(string $referencia): self
+    {
+        $this->referencia = $referencia;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getPrecio(): ?float
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(float $precio): self
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+}
